64466d3291676eccfba852c0d0a7379376c213f1
Aleksandr Cherevko <cherevko.alexandr.1997@gmail.com>
2020-08-29 16:50:34 +0300

    fix task3

--- src/chrvk/task3/Main.java
+++ src/chrvk/task3/Main.java
@@ -7,15 +7,15 @@ package chrvk.task3;
 public class Main {
     public static final byte size = 30;
 
-    public static String CreateLine(){
+    public static String createLine() {
         StringBuilder line = new StringBuilder();
-        for(int i = 0; i < size; i++){
-            line.append('(').append(i).append(')').append(" ");
+        for (int i = 0; i < size; i++) {
+            line.append('(').append(i + 1).append(')').append(" ");
         }
         return line.toString();
     }
 
     public static void main(String[] args) {
-        System.out.println(CreateLine());
+        System.out.println(createLine());
     }
 }
